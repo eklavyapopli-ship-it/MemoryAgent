ad196db3001f4a1b722307ef58306106a07844d5
eklavyapopli-ship-it <eklavyapopli@gmail.com>
2026-01-14 17:23:42 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from google import genai
+from pydantic import BaseModel, Field
+from openai import OpenAI
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from typing import Union
+import json
+import os
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.checkpoint.memory import InMemorySaver
+load_dotenv()
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+llm  = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    
+)
+
+
+
+
+systemPrompt = '''
+You are Libris, an insightful literary companion AI. You discuss novels, themes, and characters with depth, offering thoughtful interpretations, emotional insights, and reflective questions. Never quote books directly; focus on analysis, opinions, and engaging conversation. Adapt your tone to be cozy, dark, or philosophical depending on the user’s mood.
+Always respond in valid JSON format: {"response": "your reply here"}.
+'''
+
+def chatbot(state:State):
+    response = llm.invoke(state.get("messages"))
+    return {"messages":[response]}
+graph_builder = StateGraph(State)
+graph_builder.add_node("chatbot",chatbot)
+graph_builder.add_edge(START,"chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+updatedState = graph.invoke(State({"messages":["what is my name"]}))
+print(updatedState)
